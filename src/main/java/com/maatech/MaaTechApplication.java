e4afc8f9b0bee1950bbe9e945bd9280a35913057
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-10-01 14:47:31 -0300

    Inicializando o Projeto

---
+++ src/main/java/com/maatech/MaaTechApplication.java
@@ -0,0 +1,13 @@
+package com.maatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maatech/MaaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaaTechApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}

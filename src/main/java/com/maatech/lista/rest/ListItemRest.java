46716d2627d18999a3be7d60f7b8ad37a321c93f
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-10-12 15:59:23 -0300

    Criando corpo das classes REST ListItem

--- src/main/java/com/maatech/lista/rest/ListItemRest.java
+++ src/main/java/com/maatech/lista/rest/ListItemRest.java
@@ -1,4 +1,14 @@
 package com.maatech.lista.rest;
 
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ListItemRest {
+
+    List<ResponseEntity<?>> findListByIdUser(UUID idUser);
+    List<ResponseEntity<?>> listAllLitsItems();
+    ResponseEntity<?> addNewItemToList(UUID idUser, UUID idItem);
+    ResponseEntity<?> deleteItemFromUserList(UUID idUser, UUID idItem);
 }
--- src/main/java/com/maatech/lista/rest/ListItemRestImp.java
+++ src/main/java/com/maatech/lista/rest/ListItemRestImp.java
@@ -1,4 +1,28 @@
 package com.maatech.lista.rest;
 
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
+
 public class ListItemRestImp implements ListItemRest{
+    @Override
+    public List<ResponseEntity<?>> findListByIdUser(UUID idUser) {
+        return List.of();
+    }
+
+    @Override
+    public List<ResponseEntity<?>> listAllLitsItems() {
+        return List.of();
+    }
+
+    @Override
+    public ResponseEntity<?> addNewItemToList(UUID idUser, UUID idItem) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> deleteItemFromUserList(UUID idUser, UUID idItem) {
+        return null;
+    }
 }

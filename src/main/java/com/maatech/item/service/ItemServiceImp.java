b5c98b83e7cb91d095786403e31990ce79228eec
Paulo Henrique Leão de Oliveira <phleaoliveira@gmail.com>
2024-10-12 16:25:30 -0300

    Criando corpo das Services

--- src/main/java/com/maatech/item/service/ItemServiceImp.java
+++ src/main/java/com/maatech/item/service/ItemServiceImp.java
@@ -1,4 +1,34 @@
 package com.maatech.item.service;
 
+import com.maatech.item.entity.ItemDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
+
 public class ItemServiceImp implements ItemService{
+    @Override
+    public ResponseEntity<?> findItemById(UUID idItem) {
+        return null;
+    }
+
+    @Override
+    public List<ResponseEntity<?>> findAllItems() {
+        return List.of();
+    }
+
+    @Override
+    public ResponseEntity<?> createItem(ItemDTO item) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> updateItem(ItemDTO item) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> deleteItemById(UUID idItem) {
+        return null;
+    }
 }

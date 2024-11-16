dabc5c58c2c223066266b0da1439f15e744d5e1a
paulinnleao <phleaoliveira@gmail.com>
2024-11-16 10:23:56 -0300

    Listagem dos produtos

--- frontend/src/components/pages/ResultPage.tsx
+++ frontend/src/components/pages/ResultPage.tsx
@@ -1,24 +1,30 @@
-import React from "react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 import { CardList, CardProps } from "../item/CardList";
+import { useNavigate } from "react-router-dom";
 
 interface ResultPageProps {
     listResult: CardProps[];
 }
 
-const ResultPage: React.FC<ResultPageProps> = ({ listResult }) => {
+// const ResultPage: React.FC<ResultPageProps | null> = ({ listResult }) => {
+const ResultPage = () => {
     const testeImagem = {
         src: "https://wallpapers.com/images/high/goku-pictures-im28xdck0mbth703.webp",
         alt: "goku vermei",
         title: "GOKU VERMEI",
         description: "TESTANDO O GOKU VERMEI"
     } 
+    const listResult = Array.from({length: 5}, () => testeImagem);
+    const navigate = useNavigate();
     return (
-        <>
-        <CardList {...testeImagem} />
-            {/* {listResult.map((item, index) => (
-                <CardList key={index} {...item} />
-            ))} */}
-        </>
+        <Box>
+            <Button  onClick={()=>navigate("submission-form")}>Voltar</Button>
+            <Flex wrap={"wrap"} gap={"2rem"}>
+                {listResult.map((item, index) => (
+                    <CardList key={index} {...item} />
+                ))}
+            </Flex>
+        </Box>
     );
 };
 
